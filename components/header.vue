6d87e95e69190cc7a0495a209a530d7fd8c77cae
nakagawa <>
2019-07-26 18:56:31 +0900

    不要なCSS定義を削除, navMenu を右寄せ

--- components/header.vue
+++ components/header.vue
@@ -23,7 +23,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
             <b-nav-item href="/">
               <div class="mx-auto" style="width: 120px;">
                 <img
@@ -109,94 +109,6 @@ export default {
   color: #463100;
   text-decoration: underline;
   padding: 0px;
-}
-
-/*--------------------------------------------------------
-グローバルメニュー
---------------------------------------------------------*/
-
-#global-menu {
-  float: right;
-  height: 78px;
-  margin-top: 17px;
-  margin-right: 20px;
-}
-
-#global-menu ul {
-  position: relative;
-  height: 78px;
-  list-style: none;
-}
-
-#global-menu li {
-  float: left;
-  width: 120px;
-  height: 78px;
-  overflow: hidden;
-  border-left: 1px dotted #463100;
-  font-size: 1.4em;
-  font-weight: bold;
-  line-height: 1.2;
-}
-
-#global-menu li.nav-last {
-  border-right: 1px dotted #463100;
-}
-
-#global-menu li a {
-  text-indent: -9999px;
-  width: 120px;
-  height: 78px;
-  display: block;
-  text-decoration: none;
-}
-* html #global-menu ul li a {
-  height: auto;
-}
-
-#global-menu ul #nav-home {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_home_off.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-shinkan {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_special_off.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-about {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_about_us_off.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-members {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_members_off.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-link {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_links_off.gif)
-    left top no-repeat;
-}
-
-#global-menu ul #nav-home:hover {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_home_on.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-shinkan:hover {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_special_on.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-about:hover {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_about_us_on.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-members:hover {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_members_on.gif)
-    left top no-repeat;
-}
-#global-menu ul #nav-link:hover {
-  background: url(https://ku-unplugged.net/wp/wp-content/themes/keni_light_wp/images/common/global_navi_links_on.gif)
-    left top no-repeat;
-}
-* html #global-menu ul li a:hover {
-  height: auto;
 }
 </style>
 
