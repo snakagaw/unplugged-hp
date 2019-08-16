dc341bd2eb0d6ea7627846ad6e3c25c63acc04ec
nakagawa <>
2019-08-16 23:44:44 +0900

    リンク追加

---
+++ pages/links.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <h1>リンク</h1>
+    <ul>
+      <li v-for="(link, key) in links" :key="key">
+        <a :href="link.url" target="_blank">{{link.name}}</a>
+        {{ link.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import links from '~/contents/links.json'
+export default {
+  data() {
+    return {
+      links: links
+    }
+  }
+}
+</script>
