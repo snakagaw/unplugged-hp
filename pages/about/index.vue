b006a1b8133c18a2ebfe293d600f7b9dc029fd1d
nakagawa <>
2019-08-16 18:34:40 +0900

    about にfontawesome アイコンを追加

---
+++ pages/about/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    <h1>サークル紹介</h1>
+    <h2>“アンプラグド”とは</h2>
+    <p>
+      サークル名の「アンプラグド」とはそもそもどういう意味なのか。
+      <br />このサークルが創設された理由から目的までを説明します。
+    </p>
+    <p class="link-next">
+      <a href="what_is_unplugged/"><font-awesome-icon icon="arrow-alt-circle-right"/>“アンプラグド”とは</a>
+    </p>
+    <h2>活動紹介</h2>
+    <p>
+      京大アンプラグドがいつ、どこで、どんなふうに活動しているのか。
+      <br />例会、ライブ、その他イベントについて説明します。
+    </p>
+    <p class="link-next">
+      <a href="activities/"><font-awesome-icon icon="arrow-alt-circle-right"/>活動紹介</a>
+    </p>
+    <h2>入会するには</h2>
+    <p>
+      京大アンプラグドに入会するにはどうすればいいのか。
+      <br />どういうメンバーがアンプラグドに入っているのか説明します。
+    </p>
+    <p class="link-next">
+      <a href="membership/"><font-awesome-icon icon="arrow-alt-circle-right"/>入会するには</a>
+    </p>
+  </div>
+</template>
