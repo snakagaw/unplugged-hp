1a11d0b71fd4c88d8051d9dd8888820da3bf4f7a
nakagawa <>
2019-12-16 01:32:14 +0900

    リンクを修正

--- pages/links.vue
+++ pages/links.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="main">
     <h1>リンク</h1>
-    <ul>
+    <ul class="links">
       <li v-for="(link, key) in links" :key="key">
-        <a :href="link.url" target="_blank">{{link.name}}</a>
-        {{ link.description }}
+        <h3>
+          <a :href="link.url" target="_blank">{{ link.name }}</a>
+        </h3>
+        <div>
+          {{ link.description }}
+        </div>
       </li>
     </ul>
   </div>
@@ -19,3 +23,32 @@ export default {
   }
 }
 </script>
+<style scoped>
+.links {
+  list-style: none;
+}
+ul {
+  padding: 0px;
+}
+.links h3 {
+  margin-top: 20px;
+  margin-bottom: 0px;
+  padding: 2px 8px;
+  background-color: #ffffdd;
+  border: 3px solid #ffeeaa;
+  color: #463100;
+  font-size: 1.3em;
+  font-weight: bold;
+}
+.links div {
+  margin-left: 1px;
+  margin-right: 1px;
+  padding: 16px;
+  border-left: 1px solid #000000;
+  border-bottom: 1px solid #000000;
+  border-right: 1px solid #000000;
+}
+.list {
+  padding-left: 20px;
+}
+</style>
