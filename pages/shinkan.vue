403b97b76e74df1a95d32860d3b27b6d4c0cf2ff
htsume <hashimasa0503@gmail.com>
2020-03-31 13:29:42 +0900

    新歓ページにビラを掲載

--- pages/shinkan.vue
+++ pages/shinkan.vue
@@ -4,8 +4,17 @@
     <p>
       2020年度、前期の新歓の詳細については、コロナウイルス流行の影響により、現在(2020/3/28)協議中です。<br>
       サークル内での決定の後にTwitterと、このページでお知らせします。
-      <br />
-      <br />お問い合わせの方は
+    </p>
+    <h2>新歓ビラ2020</h2>
+    <img src="/uploads/2020_flyer.jpg" alt="" title="新歓ビラ2020" class="img-fluid" />
+    <p>
+      京都大学からの新歓自粛要請を踏まえ、アンプラは予定していたビラ配布を中止します…<br>
+      5月から新歓を再開する予定ですが、状況によっては変更する場合もあるので、<br>
+      随時変更をtwitterやHPで確認してください！！<br>
+      （ビラの内容は2020/3/31現在のものです）
+    </p>
+    <p>
+      お問い合わせの方は
       <a href="mailto:info@ku-unplugged.net">メール(info@ku-unplugged.net)</a>
       <br />
       <br />又はtwitter[DM] までお気軽に連絡して下さい!
@@ -18,19 +27,19 @@
         data-size="large"
       >@kyodaiunpluggedをフォロー</a>
 
-      <script>
-  !(function(d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0],
-      p = /^http:/.test(d.location) ? 'http' : 'https'
-    if (!d.getElementById(id)) {
-      js = d.createElement(s)
-      js.id = id
-      js.src = p + '://platform.twitter.com/widgets.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    }
-  })(document, 'script', 'twitter-wjs')
-      </script>
     </p>
+      <script>
+      !(function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0],
+          p = /^http:/.test(d.location) ? 'http' : 'https'
+        if (!d.getElementById(id)) {
+          js = d.createElement(s)
+          js.id = id
+          js.src = p + '://platform.twitter.com/widgets.js'
+          fjs.parentNode.insertBefore(js, fjs)
+        }
+      })(document, 'script', 'twitter-wjs')
+      </script>
   </div>
 </template>
