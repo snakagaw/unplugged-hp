5433da2ed8b0cf90f313d0f54114af0b3f2e7932
sinnta123 <49526743+sinnta123@users.noreply.github.com>
2023-03-30 02:32:37 +0900

    Update members.vue

--- pages/members.vue
+++ pages/members.vue
@@ -1,66 +1,17 @@
 <template>
   <div class="main">
     <h1>会員</h1>
-    <h2>例会教室お知らせについて</h2>
+    
+    <h2>LiveLog</h2>
     <p>
-      <a href="https://message.ku-unplugged.net/meeting_room/" target="_blank">今月の例会教室はこちら</a>
+      <a href="https://livelog.ku-unplugged.net" target="_blank">LiveLog</a>
     </p>
-    <h2>ピアノシフトについて</h2>
+    
+    <h2>Zoomail</h2>
     <p>
-      セキュリティの都合上,ピアノシフトの運営方式が変更されました.
-      ピアノシフトのカレンダーを個人のカレンダーに共有する方式です.
-      <br />
-      <br />Gmail のメールアドレスから,
-      <a href="mailto:unpluggedpiano@gmail.com">unpluggedpiano@gmail.com</a
-      >に名前と回生を書いて件名を「ピアノシフト」としてメールを送ってください.
-      <br />
-      <br />「Google カレンダー “ピアノシフト”
-      での予定の編集が許可された」との旨のメールが届きます(すこし時間がかかる場合があります).
-      <br />
-      <br />その後, 送信したGmailのGoogle Calendar の 【他のカレンダー】に,
-      ピアノシフトが表示されるようになります. シフトの入れ方などは,
-      以前と同じです.
-      <br />
-      <br />ご自分のカレンダーとピアノシフトのカレンダーを間違えないようご注意ください.
-      <br />
-      <br />また,
-      シフトを追加すると、そのシフトを取った人のアドレスが表示されることになります。
-      <br />
-      <br />ご質問などがありましたら,
-      <a href="mailto:unpluggedpiano@gmail.com">unpluggedpiano@gmail.com</a>
-      までご連絡をいただきますようお願いします.
+      <a href="https://message.ku-unplugged.net/player/" target="_blank">Zoomail</a>
     </p>
-    <h2>ライブ音源</h2>
-    <p>
-      <a href="https://livelog.ku-unplugged.net" target="_blank">LiveLog</a>でご確認ください.
-    </p>
-    <h2>ライブリハ音源</h2>
-    <!-- TODO:名前直す -->
-    <p>
-      <a href="https://message.ku-unplugged.net/player/" target="_blank"
-        >アンプラ会員向けサイト(リハ音源)</a
-      >でご確認ください.
-    </p>
-    <h2>ライブ動画</h2>
-    <p>
-      <a href="https://livelog.ku-unplugged.net" target="_blank">LiveLog</a>でご確認ください.
-      <br />LiveLogに掲載されていない映像は
-      <a href="https://message.ku-unplugged.net/movie/" target="_blank"
-        >アンプラ会員向けサイト(ライブ映像)からどうぞ.</a
-      >
-    </p>
-    <h2>各種書類</h2>
-    <p>
-      <a href="https://message.ku-unplugged.net/others/" target="_blank"
-        >アンプラ会員向けサイト(その他資料)</a
-      >でご確認ください.
-    </p>
-    <h2>感想用紙</h2>
-    <p>
-      <a href="https://message.ku-unplugged.net/kansou/" target="_blank"
-        >アンプラ会員向けサイト(感想用紙)</a
-      >でご確認ください.
-    </p>
+    
     <h2>twitter</h2>
     <p>
       <a
@@ -77,7 +28,5 @@
         charset="utf-8"
       ></script>
     </p>
-    <h2>メーリングリスト</h2>
-    <p><a href="https://message.ku-unplugged.net/" target="_blank">移行しました.</a>登録は<a href="https://forms.gle/JGfCRzohMeDELvmd6" target="_blank">こちら</a></p>
   </div>
 </template>
