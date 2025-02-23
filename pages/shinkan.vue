5cc97003ec9c52384f1ec39d59277e5d884091ff
HC-Gappy <kachikachin.0912@gmail.com>
2025-02-23 22:41:39 +0900

    2025年度の更新待ち状態に変更
    新歓フォーム
    各種新歓アカウント
    新歓予定カレンダーを随時更新していく
    
    一旦古いリンク類を非表示にしているので、リンク整い次第更新

--- pages/shinkan.vue
+++ pages/shinkan.vue
@@ -2,9 +2,12 @@
   <div class="main">
     <h1>新歓特設ページ</h1>
     <p>
-      <b>※新歓に参加される方は、
-      <a href="https://forms.gle/ntiKuQe8pS1hK4sg7">こちら</a>
-      のフォームに回答してLINEオープンチャットに参加してください！</b>
+      ただいま準備中です...
+      <!--2025年度更新待ち
+        <b>※新歓に参加される方は、
+        <a href="https://forms.gle/ntiKuQe8pS1hK4sg7">こちら</a>
+        のフォームに回答してLINEオープンチャットに参加してください！</b>
+      -->
     </p>
     <h2>2025年度新歓日程</h2>
     <p>
@@ -18,33 +21,50 @@
 
     <h2>2024年度新歓イベント</h2>
     <p>
-      今年の京大アンプラグドでは、以下の新歓イベントを開催します！
+      2024年度は、以下の新歓イベントを開催しました！<br>
+      2025年度の新歓イベントも乞うご期待！
     </p>
     <p>
       新歓ライブ @4共・学生集会所3階 … アンプラグドの会員による演奏を披露します！<br>
       楽器交流会 @4共 … 先輩達に教えてもらいながら、楽器を体験できます！<br>
-      花見 @鴨川 … 音楽を楽しみながら、楽しくお花見をしましょう！ 参加フォームは
-      <a href="https://docs.google.com/forms/d/1BdMu5xARTH2-18e-CwdcUzThvBS1Ptj3hL4ely9vGK4/viewform">こちら</a><br>
+      花見 @鴨川 … 音楽を楽しみながら、楽しくお花見をしましょう！ 
+      
+      <!--2025年度更新待ち
+        参加フォームは
+        <a href="https://docs.google.com/forms/d/1BdMu5xARTH2-18e-CwdcUzThvBS1Ptj3hL4ely9vGK4/viewform">こちら</a>
+      -->
+      <br>
+
       ビラ配り @クスノキ前 … 紅萠祭のビラロードでビラ配りをします！<br>
     </p>
     <p>
       その他、他団体とのコラボライブもあります！ 詳しくは公式SNSをチェック↓<br>
-      新歓特設Xアカウント→
-      <a
-        class="twitter-follow-button"
-        href="https://twitter.com/unplugged2024"
-        data-show-count="false"
-        data-lang="ja"
-        data-size="large"
-      >@unplugged2024</a><br>
-      新歓特設Instagramアカウント→
-      <a href="https://www.instagram.com/unplugged_kyodai2024/">@unplugged_kyodai2024</a><br>
+      新歓特設Xアカウント→準備中
+      <!--2025年度更新待ち
+        <a
+          class="twitter-follow-button"
+          href="https://twitter.com/unplugged2024"
+          data-show-count="false"
+          data-lang="ja"
+          data-size="large"
+          >@unplugged2024
+        </a>
+      -->
+      
+      <br>
+      新歓特設Instagramアカウント→準備中
+      <!--2025年度更新待ち
+        <a href="https://www.instagram.com/unplugged_kyodai2024/">@unplugged_kyodai2024</a>
+      -->
+      <br>
     </p>
+    
     <h2>ライブ動画</h2>
     <p>
       アンプラグドで過去に行われたライブのうち、いくつかを公開しています！<br>
       動画ページは<nuxt-link to="/movie">こちら</nuxt-link>！<br>
     </p>
+    
     <h2>お問い合わせ</h2>
     <p>
       何か気になることがある方は
@@ -54,8 +74,8 @@
       ・
       <a href="https://instagram.com/unplugged_kyodai2024/">instagram</a>
       のDMまでお気軽に連絡して下さい!<br>
-      
     </p>
+    
       <script>
       !(function(d, s, id) {
         var js,
